a335fec0e628eb0879f2d88f68ca54c4ed1d74c4
Last-SilverLight-End <68593892+Last-SilverLight-End@users.noreply.github.com>
2023-01-06 23:39:09 +0900

    feat : inital Settings

---
+++ HAZACommunity/HAZACommunity/src/main/java/HAZAGroup/HAZACommunity/HazaCommunityApplication.java
@@ -0,0 +1,13 @@
+package HAZAGroup.HAZACommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HazaCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HazaCommunityApplication.class, args);
+	}
+
+}
---
+++ HAZACommunity/HAZACommunity/src/test/java/HAZAGroup/HAZACommunity/HazaCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package HAZAGroup.HAZACommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HazaCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
