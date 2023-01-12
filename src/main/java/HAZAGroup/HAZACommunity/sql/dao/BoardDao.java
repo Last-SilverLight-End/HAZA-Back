1e6fcb67d81ecb96bf193c6f71e4a8c66a5e78ad
Kim HyunJae <duct1235@gmail.com>
2023-01-13 01:13:09 +0900

    db 연결 테스트용 GET Rest Api 추가

--- src/main/java/HAZAGroup/HAZACommunity/sql/dao/BoardDao.java
+++ src/main/java/HAZAGroup/HAZACommunity/sql/dao/BoardDao.java
@@ -1,4 +1,12 @@
 package HAZAGroup.HAZACommunity.sql.dao;
 
+import HAZAGroup.HAZACommunity.board.dto.BoardDto;
+import org.apache.ibatis.session.SqlSession;
+
+import java.util.List;
+
 public class BoardDao {
+    public List<BoardDto> getBoardList(SqlSession sqlSession) throws Exception{
+        return sqlSession.selectList("board.getBoardList");
+    }
 }
