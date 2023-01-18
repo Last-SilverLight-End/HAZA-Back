dbbdb01874a331bce7f886f7260f3cf79100a845
Last-SilverLight-End <68593892+Last-SilverLight-End@users.noreply.github.com>
2023-01-18 19:25:25 +0900

    feat : add member

---
+++ src/main/java/HAZAGroup/HAZACommunity/rest/board/controller/UserController.java
@@ -0,0 +1,36 @@
+package HAZAGroup.HAZACommunity.rest.board.controller;
+
+import HAZAGroup.HAZACommunity.common.response.model.BasicResponse;
+import HAZAGroup.HAZACommunity.common.response.model.CommonResponse;
+import HAZAGroup.HAZACommunity.common.response.model.ErrorResponse;
+import HAZAGroup.HAZACommunity.rest.board.model.UserVo;
+import HAZAGroup.HAZACommunity.rest.board.service.UserService;
+import HAZAGroup.HAZACommunity.sql.dao.UserDao;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+@RequestMapping("api/users")
+public class UserController {
+
+    private UserService userService;
+
+    @GetMapping(produces = "application/json;charset=UTF-8")
+    public ResponseEntity<BasicResponse> getDrawingUserInform() throws Exception{
+        CommonResponse<UserVo> commonResponse;
+
+        try{
+            commonResponse = new CommonResponse<UserVo>(userService.getUserSample());
+            commonResponse.setStatus(200);
+
+            return ResponseEntity.ok().body(commonResponse);
+        }
+        catch (Exception e){
+            return ResponseEntity.internalServerError().body(new ErrorResponse("유저 정보 불러오기 실패", HttpStatus.INTERNAL_SERVER_ERROR.value()));
+        }
+    }
+}
