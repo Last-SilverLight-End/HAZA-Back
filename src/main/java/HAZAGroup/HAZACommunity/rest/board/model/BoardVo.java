512545f85c90777bbbe9a111732e111213b08963
Last-SilverLight-End <68593892+Last-SilverLight-End@users.noreply.github.com>
2023-01-19 18:23:04 +0900

    feat : board 정리 및 dao list modifying 작업중

--- src/main/java/HAZAGroup/HAZACommunity/rest/board/model/BoardVo.java
+++ src/main/java/HAZAGroup/HAZACommunity/rest/board/model/BoardVo.java
@@ -11,15 +11,26 @@ import lombok.Setter;
 @RequiredArgsConstructor
 public class BoardVo {
     @NonNull
+    private int board_id;
+    @NonNull
     private String title;
     @NonNull
     private String content;
+    @NonNull
     private int hit;
+    @NonNull
+    private String created_date;
+    @NonNull
+    private String modified_date;
+    @NonNull
+    private int user_id;
     @NonNull
     private String user_name;
     @NonNull
     private String user_email;
-    private String mainCategory;
-    private String midCategory;
+
+    private String main_Category_Id;
+    private String mid_Category_Id;
+
 
 }
