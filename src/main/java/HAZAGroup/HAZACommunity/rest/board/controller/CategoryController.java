ca44fe37440e9c2e6620c4c717a3b038d7eb26e8
Last-SilverLight-End <68593892+Last-SilverLight-End@users.noreply.github.com>
2023-01-31 19:14:47 +0900

    fix board api , need to fix category map

--- src/main/java/HAZAGroup/HAZACommunity/rest/board/controller/CategoryController.java
+++ src/main/java/HAZAGroup/HAZACommunity/rest/board/controller/CategoryController.java
@@ -4,14 +4,19 @@ import HAZAGroup.HAZACommunity.common.response.model.BasicResponse;
 import HAZAGroup.HAZACommunity.common.response.model.CommonResponse;
 import HAZAGroup.HAZACommunity.common.response.model.ErrorResponse;
 import HAZAGroup.HAZACommunity.rest.board.model.GenreMainCategoryVo;
+import HAZAGroup.HAZACommunity.rest.board.model.GenreMidCategoryVo;
 import HAZAGroup.HAZACommunity.rest.board.service.CategoryService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.apache.catalina.connector.Response;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.HandlerMapping;
 
 import java.util.HashMap;
 import java.util.List;
@@ -19,21 +24,25 @@ import java.util.Map;
 
 @Controller
 @Mapper
-@RequestMapping("/api/boards")
+@RequestMapping("/api/category")
 public class CategoryController {
 
     @Autowired
     private CategoryService categoryService;
 
-    @RequestMapping(value = "/", produces = "application/json;charset=UTF-8" )
-    public ResponseEntity<BasicResponse> getAllGenres(String main, String sub) throws Exception{
+    @RequestMapping(value = "", method = RequestMethod.GET, produces = "application/json;charset=UTF-8" )
+//    public ResponseEntity<BasicResponse> getAllGenres(HttpSession session, HttpServletRequest request, ModelMap modelMap) throws Exception{
+//        String requestUrl = (String)request.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE);
+//        Map<String, Object> map = new HashMap<String, Object>();
+        public @ResponseBody
+        ResponseEntity<BasicResponse> getAllGenres(@RequestParam String main,@RequestParam String sub) throws Exception{
         Map<String,Object> map = new HashMap<String,Object>();
         map.put("main",main);
         map.put("sub",sub);
-        CommonResponse<List<GenreMainCategoryVo>> commonResponse;
+        CommonResponse<List<GenreMidCategoryVo>> commonResponse;
         try{
 
-            commonResponse = new CommonResponse<>(categoryService.getMainCategoryStatus(map));
+            commonResponse = new CommonResponse<>(categoryService.getMidCategoryStatus(map));
             commonResponse.setStatus(200);
 
             return ResponseEntity.ok().body(commonResponse);
