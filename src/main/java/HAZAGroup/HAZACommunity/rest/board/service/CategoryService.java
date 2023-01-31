54713ca1a1c2ab3e6ee332db2e848b81d515c19a
Last-SilverLight-End <68593892+Last-SilverLight-End@users.noreply.github.com>
2023-01-31 13:13:42 +0900

    feat : 종규 카테로기 전체 가져오기 생성

---
+++ src/main/java/HAZAGroup/HAZACommunity/rest/board/service/CategoryService.java
@@ -0,0 +1,58 @@
+package HAZAGroup.HAZACommunity.rest.board.service;
+
+import HAZAGroup.HAZACommunity.rest.board.model.GenreMainCategoryVo;
+import HAZAGroup.HAZACommunity.rest.board.model.GenreMidCategoryVo;
+import HAZAGroup.HAZACommunity.sql.SqlSessionManager;
+import HAZAGroup.HAZACommunity.sql.dao.CategoryDao;
+import org.apache.ibatis.session.SqlSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class CategoryService {
+
+    Logger logger = LoggerFactory.getLogger(CategoryService.class);
+    SqlSession sqlSession = null;
+
+    public List<GenreMainCategoryVo> getMainCategoryStatus(Map<String, Object> map) throws Exception{
+
+        try{
+            CategoryDao categoryDao = new CategoryDao();
+            SqlSessionManager sqlSessionManager = new SqlSessionManager();
+            sqlSession = sqlSessionManager.getSqlSession();
+
+            return categoryDao.getMainCategoryLists(sqlSession);
+
+        } catch (Exception e){
+            e.printStackTrace();
+            logger.error(e.getMessage());
+            throw e;
+        }
+        finally {
+            sqlSession.close();
+        }
+    }
+
+    public List<GenreMidCategoryVo> getMidCategoryStatus(Map<String, Object> map) throws Exception{
+        try{
+            CategoryDao categoryDao = new CategoryDao();
+            SqlSessionManager sqlSessionManager = new SqlSessionManager();
+            sqlSession = sqlSessionManager.getSqlSession();
+
+            return categoryDao.getMidCategoryLists(map,sqlSession);
+
+        } catch (Exception e){
+            e.printStackTrace();
+            logger.error(e.getMessage());
+            throw e;
+        }
+
+        finally {
+            sqlSession.close();
+        }
+    }
+}
