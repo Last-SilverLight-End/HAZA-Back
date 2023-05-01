f5dc236ff7b32bf82481dc4477679f9d8c1d5f93
Last-SilverLight-End <halfwing1998@gmail.com>
2023-05-01 18:45:57 +0900

    feat : comments 추가

---
+++ src/main/java/HAZAGroup/HAZACommunity/rest/board/controller/CommentController.java
@@ -0,0 +1,16 @@
+package HAZAGroup.HAZACommunity.rest.board.controller;
+
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Mapper
+@RequestMapping("/api/comments")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class CommentController {
+
+    private final
+}
