b35e4b71e58e62b48d0be65cb4e516f18f679441
Last-SilverLight-End <halfwing1998@gmail.com>
2023-05-07 17:47:12 +0900

    feat  comment Controller,Service 작성

--- src/main/java/HAZAGroup/HAZACommunity/sql/dao/CommentDao.java
+++ src/main/java/HAZAGroup/HAZACommunity/sql/dao/CommentDao.java
@@ -1,4 +1,17 @@
 package HAZAGroup.HAZACommunity.sql.dao;
 
+import HAZAGroup.HAZACommunity.rest.board.model.CommentVo;
+import org.apache.ibatis.session.SqlSession;
+
+import java.util.List;
+
 public class CommentDao {
+
+    public List<CommentVo> getCommentLists (SqlSession sqlSession) throws Exception {
+        System.out.println("show all comment Lists");
+        List<CommentVo> allCommentLists = sqlSession.selectList("comment.getAllCommentLists");
+        System.out.println("allCommentLists = " + allCommentLists);
+        return allCommentLists;
+    }
+
 }
