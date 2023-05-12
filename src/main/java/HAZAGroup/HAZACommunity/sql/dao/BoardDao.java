f04a02882d20e422f334e4646665542e39a7166c
Liruns <83442060+Liruns@users.noreply.github.com>
2023-05-12 14:29:44 +0900

    Update BoardDao.java

--- src/main/java/HAZAGroup/HAZACommunity/sql/dao/BoardDao.java
+++ src/main/java/HAZAGroup/HAZACommunity/sql/dao/BoardDao.java
@@ -2,52 +2,51 @@ package HAZAGroup.HAZACommunity.sql.dao;
 
 import HAZAGroup.HAZACommunity.rest.board.model.BoardVo;
 import org.apache.ibatis.session.SqlSession;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import javax.sql.DataSource;
 import java.util.List;
 
 @Repository
 public class BoardDao {
-    @Autowired
-    private DataSource dataSource;
-    @Autowired
-    private SqlSession sqlSession;
+    // delete board
+    public static String deleteBoardList(int id, SqlSession sqlSession) throws Exception {
+        System.out.println("id = " + id);
+        return sqlSession.selectList("board.deleteBoardList", id).toString();
+    }
+
+    // create board
+    public static int insertBoardList(BoardVo boardVo, SqlSession sqlSession) throws Exception {
+        System.out.println("boardVo = " + boardVo);
+        return sqlSession.insert("board.insertBoardList", boardVo);
+    }
 
     // all board data
     public List<BoardVo> getBoardLists(SqlSession sqlSession) throws Exception {
         System.out.println(" dao sqlSession = " + sqlSession);
         return sqlSession.selectList("board.getBoardLists");
     }
+
     // board mainCategory id lists
     public List<BoardVo> getSpecificMainCategoryBoardLists(int mainCategoryId, SqlSession sqlSession) throws Exception {
         System.out.println("mainCategoryId = " + mainCategoryId);
         return sqlSession.selectList("board.getSpecificMainCategoryBoardLists", mainCategoryId);
     }
+
     // board detail id
     public List<BoardVo> getSpecificBoardLists(int id, SqlSession sqlSession) throws Exception {
         System.out.println("id = " + id);
         return sqlSession.selectList("board.getSpecificBoardLists", id);
     }
+
     // delete board
     public int getDeleteBoardLists(int id, SqlSession sqlSession) throws Exception {
         System.out.println("id = " + id);
         return sqlSession.delete("board.getDeleteBoard", id);
     }
+
     // modify board
     public List<BoardVo> modifyBoardList(int id, SqlSession sqlSession) throws Exception {
         System.out.println("id = " + id);
         return sqlSession.selectList("board.modifyBoardList", id);
-    }
-    // delete board
-    public static String deleteBoardList(int id, SqlSession sqlSession) throws Exception {
-        System.out.println("id = " + id);
-        return sqlSession.selectList("board.deleteBoardList", id).toString();
-    }
-    // create board
-    public static int insertBoardList(BoardVo boardVo,SqlSession sqlSession) throws Exception{
-        System.out.println("boardVo = " + boardVo);
-        return sqlSession.insert("board.insertBoardList",boardVo);
     }
 }
