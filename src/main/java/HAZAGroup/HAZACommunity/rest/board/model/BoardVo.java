f361117f2cd6a90de1c6203a1215bcbbd4ebaff8
Liruns <83442060+Liruns@users.noreply.github.com>
2023-05-12 14:29:26 +0900

    Update BoardVo.java

--- src/main/java/HAZAGroup/HAZACommunity/rest/board/model/BoardVo.java
+++ src/main/java/HAZAGroup/HAZACommunity/rest/board/model/BoardVo.java
@@ -2,7 +2,6 @@ package HAZAGroup.HAZACommunity.rest.board.model;
 
 
 import lombok.Getter;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
@@ -10,23 +9,22 @@ import lombok.Setter;
 @Setter
 @RequiredArgsConstructor
 public class BoardVo {
-    @NonNull
+
     private int id;
-    @NonNull
+
     private String title;
-    @NonNull
+
     private String content;
-    @NonNull
+
     private int hit;
-    @NonNull
+
     private String createdDate;
-    @NonNull
+
     private String modifiedDate;
-    @NonNull
+
     private int userId;
-    @NonNull
+
     private String userName;
-    @NonNull
     private String userEmail;
 
     private String mainCategoryId;
